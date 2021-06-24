d871d542d38b76ff91588e5ac05d493fc4651882
yak-fumblepack <liang.mike.to@gmail.com>
2021-06-23 21:12:18 -0400

    fix dark theme and toggling issues

--- static/darkMode.js
+++ static/darkMode.js
@@ -22,6 +22,44 @@ const getCookie = (name) => {
 if (getCookie("isDark") == 1) {
   document.body.classList.add("dark")
   try {
+    let user_list = document.getElementsByClassName("users-list-group-item")
+    for (let i = 0; i < user_list.length; i++) {
+      user_list[i].classList.add("bg-dark")
+      user_list[i].classList.add("text-white")
+    }
+  } catch (e) { console.log(e) }
+
+  try {
+    let link_list = document.getElementsByClassName("paginator-page-link-active")
+    for (let i = 0; i < link_list.length; i++) {
+      link_list[i].classList.add("bg-dark")
+      link_list[i].classList.add("text-white")
+    }
+  } catch (e) { console.log(e) }
+
+  try {
+    let link_list = document.getElementsByClassName("paginator-page-link")
+    for (let i = 0; i < link_list.length; i++) {
+      link_list[i].classList.add("bg-dark")
+      link_list[i].classList.add("text-white")
+    }
+  } catch (e) { console.log(e) }
+
+
+
+  try {
+    document.getElementById("users-tab").classList.add("bg-dark")
+    document.getElementById("users-tab").classList.add("text-white")
+  } catch (e) { console.log(e) }
+  try {
+    document.getElementById("docs-tab").classList.add("bg-dark")
+    document.getElementById("docs-tab").classList.add("text-white")
+  } catch (e) { console.log(e) }
+  try {
+    document.getElementById("getting-started-tab").classList.add("bg-dark")
+    document.getElementById("getting-started-tab").classList.add("text-white")
+  } catch (e) { console.log(e) }
+  try {
     document.getElementById("footer").classList.add("dark-footer")
   } catch (e) { console.log(e) }
   try {
@@ -40,6 +78,46 @@ if (getCookie("isDark") == 1) {
 
 const toggleDark = () => {
   document.body.classList.toggle("dark")
+  try {
+    let user_list = document.getElementsByClassName("users-list-group-item")
+    for (let i = 0; i < user_list.length; i++) {
+      user_list[i].classList.toggle("bg-dark")
+      user_list[i].classList.toggle("text-white")
+    }
+  } catch (e) { console.log(e) }
+
+  try {
+    let link_list = document.getElementsByClassName("paginator-page-link-active")
+    for (let i = 0; i < link_list.length; i++) {
+      link_list[i].classList.toggle("bg-dark")
+      link_list[i].classList.toggle("text-white")
+    }
+  } catch (e) { console.log(e) }
+
+  try {
+    let link_list = document.getElementsByClassName("paginator-page-link")
+    for (let i = 0; i < link_list.length; i++) {
+      link_list[i].classList.toggle("bg-dark")
+      link_list[i].classList.toggle("text-white")
+    }
+  } catch (e) { console.log(e) }
+
+
+
+
+
+  try {
+    document.getElementById("users-tab").classList.toggle("bg-dark")
+    document.getElementById("users-tab").classList.toggle("text-white")
+  } catch (e) { console.log(e) }
+  try {
+    document.getElementById("docs-tab").classList.toggle("bg-dark")
+    document.getElementById("docs-tab").classList.toggle("text-white")
+  } catch (e) { console.log(e) }
+  try {
+    document.getElementById("getting-started-tab").classList.toggle("bg-dark")
+    document.getElementById("getting-started-tab").classList.toggle("text-white")
+  } catch (e) { console.log(e) }
   try {
     document.getElementById("signup-btn").classList.toggle("dark-auth-buttons")
   } catch (e) { console.log(e) }
